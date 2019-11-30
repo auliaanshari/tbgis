c402c7313eb24a4b4540aaedc20f4b0434b38c60
aafathurr166 <aafathurr166@gmail.com>
2019-12-01 03:56:12 +0700

    second

--- config - Copy.php
+++ config - Copy.php
@@ -1,9 +1,9 @@
 <?php
-$host="ec2-174-129-253-140.compute-1.amazonaws.com";
-$user="scaxxhrsufmimy";
-$password="b392d1dc923932b4e06ba1670808e75795176b4f04be8017be2ac06f5f075622";
+$host="localhost";
+$user="postgres";
+$password="root";
 $port="5432";
-$dbname="d537pu4m7tm34j";
+$dbname="tb_fotokopi";
 $ttd='0b5ba19ad3f77462b202e1088b6e9081';
  
 $link= pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password) or die("Koneksi gagal");
